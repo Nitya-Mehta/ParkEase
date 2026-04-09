59dedb5f21650275120d09a1580db91a815ef03f
Nitya Mehta <85145292+Nitya-Mehta@users.noreply.github.com>
2026-04-09 06:31:32 +0530

    Allow all hosts in settings

--- ParkEase/settings.py
+++ ParkEase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b3dy+l9jbl8_33r9!#+6c88p%n!h@r7_l!pyys3pniiqc9ulx+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
