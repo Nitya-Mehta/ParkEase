58fd595296739d239e907a8b544646d7b439982a
Nitya Mehta <85145292+Nitya-Mehta@users.noreply.github.com>
2026-04-09 06:49:27 +0530

    Refactor user model retrieval and superuser creation

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .forms import CustomUserCreationForm, UserProfileForm
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
     
