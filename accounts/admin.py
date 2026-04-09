1b74d5e8792126b4ac55e22673becc3b453d18a6
Nitya Mehta <85145292+Nitya-Mehta@users.noreply.github.com>
2026-04-09 07:28:57 +0530

    Import parking models in admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
-
+from parking.models import ParkingSlot, ParkingRequest, SlotAssignment
 
 class CustomUserAdmin(UserAdmin):
     model = User
